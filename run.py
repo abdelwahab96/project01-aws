cb9f76fbde6436c71d49f4cb18b90ee79b29bdf0
abdelwahab <abdelwahabbahaa@gmail.com>
2025-07-01 23:47:28 +0300

    upgrading my code

--- run.py
+++ run.py
@@ -6,16 +6,23 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route('/save_data', methods=['GET'])
+@app.route('/save_data', methods=['GET', 'POST'])
 def save_data():
-    regno = request.args['regno']
-    name= request.args['name']
-    standard = request.args['class']
-    math = request.args['math']
-    science = request.args['science']
-    computer = request.args['computer']
-    print(regno,name,standard,math,science,computer)
-
+    if request.method == 'POST':
+        regno = request.form.get('regno', '')
+        name = request.form.get('name', '')
+        standard = request.form.get('class', '')
+        math = request.form.get('math', '')
+        science = request.form.get('science', '')
+        computer = request.form.get('computer', '')
+    else:
+        regno = request.args.get('regno', '')
+        name = request.args.get('name', '')
+        standard = request.args.get('class', '')
+        math = request.args.get('math', '')
+        science = request.args.get('science', '')
+        computer = request.args.get('computer', '')
+    
+    print(f"Data received: {regno}, {name}, {standard}, {math}, {science}, {computer}")
     return redirect(url_for('home'))
 
-app.run()
